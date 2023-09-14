731e86f1e90cf429eabeeb509ea0b5b13aa96aec
Olof Hag <olof.hag123@gmail.com>
2023-09-14 13:16:27 +0200

    Added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import About from "./pages/about.tsx"
 import Betala from "./pages/betala.tsx"
 import Frakta from "./pages/frakt.tsx"
 import Products from "./pages/products.tsx"
+import Footer from "./Footer.tsx"
+
 
 function App() {
   return (
@@ -20,6 +22,9 @@ function App() {
             <Route path="/betala" element={<Betala />} />
             <Route path="/products" element={<Products />} />
             </Routes>
+            <div>
+              <Footer/>
+            </div>
       </>
   )
 }
