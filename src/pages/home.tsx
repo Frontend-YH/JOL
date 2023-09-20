dd4da6860cc1d0164e97406af5329ca9f86af0ff
Olof Hag <olof.hag123@gmail.com>
2023-09-20 14:35:54 +0200

    Added updates to prodcard

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,26 +1,62 @@
-import About from "../components/About.tsx"
-import Footer from "../components/Footer.tsx"
-import ProdCard from "../components/ProdCard.tsx"
-import "../components/start-container.css"
-import Button from '@mui/material/Button';
+import About from "../components/About.tsx";
+import Footer from "../components/Footer.tsx";
+import ProdCard from "../components/ProdCard.tsx";
+import "../components/start-container.css";
+import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
 function Home() {
-    return ( <>
-    <div className="start-container">
-        <div className="start-text">
-        <h2>Välkommen till Gärdsjösmedja</h2>
-        <h4>Vi tillverkar allt från krokar till ljusstakar. Klicka dig in på våran butik</h4>
-        </div>
-        <Button variant="contained" size="large"className="start-btn">Till butiken</Button>
-</div>
-    <About/> 
-            <div className="products-div">
-            <ProdCard/>
-            <ProdCard/>
-            <ProdCard/>
-            </div>
+  const [data, setData] = useState([]);
 
-            <Footer/>
-    </> );
+  const getData = () => {
+    fetch("http://localhost:3000/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new Error("Kan inte hämta data");
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div className="start-container">
+        <div className="start-text">
+          <h2>Välkommen till Gärdsjösmedja</h2>
+          <h4>
+            Vi tillverkar allt från krokar till ljusstakar. Klicka dig in på
+            våran butik
+          </h4>
+        </div>
+        <Button variant="contained" size="large" className="start-btn">
+          Till butiken
+        </Button>
+      </div>
+      <About />
+
+      <div className="products-div">
+        {data.map((dataItem) => (
+          <ProdCard
+            key={dataItem._id}
+            name={dataItem.name}
+            price={dataItem.price + ":-"}
+            description={dataItem.description}
+            imgUrls={(dataItem.pictures || dataItem.picture || [])
+              .filter(Boolean)
+              .map(
+                (picture) => `https://gardsjosmedja.com/products/${picture}`
+              )}
+          />
+        ))}
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
