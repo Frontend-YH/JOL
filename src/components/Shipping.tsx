dbafe15334222148fbe421a18ffc0fad5b0e3616
Olof Hag <olof.hag123@gmail.com>
2023-09-26 10:05:30 +0200

    added required add on form

--- src/components/Shipping.tsx
+++ src/components/Shipping.tsx
@@ -1,68 +1,90 @@
 import '../shopblock.css';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
-import { ReactNode } from "react";
+import { Link } from 'react-router-dom'; // Använd bara Link här, inte CustomLink
 import { Box, TextField } from '@mui/material';
 
-
-
-interface CustomLinkProps{
-    to: string;
-    children: ReactNode;
-}
-
-
-function CustomLink({ to, children, ...props}: CustomLinkProps){
-    const resolvePath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvePath.pathname, end: true})
-
-    return (
-        <li className={isActive ? "active" : ""}>
-          <Link to={to} {...props}>
-            {children}
-          </Link>
-        </li>
-      );
-}
-
-
-
-
 function ShippingForm() {
-    return ( <div id= 'Orderingpage'>
-         <div className='Register'>
-            <h1>Vänligen ange dina uppgifter</h1>
-            <Box
-            display = "flex"
-            flexDirection= "column"
-            width="400px"
-            justifyContent="space-evenly"
-            padding="5px"
-            >
-                <TextField
-                label = "Adress"
-                sx={{ bgcolor: 'white', borderRadius: "5px", marginTop: "35px"}}
-                ></TextField>
-                <TextField
-                label = "Postnummer" 
-                sx={{ bgcolor: 'white', borderRadius: "5px",marginTop: "35px" }}
-                ></TextField>               
-                <TextField
-                label = "Ort" 
-                sx={{ bgcolor: 'white', borderRadius: "5px",marginTop: "35px", marginBottom: "127px"}}
-                ></TextField>              
-            </Box>
-<Box
-            display = "flex"
-            flexDirection= "row"
-            justifyContent="center"
-            >
+  const [address, setAddress] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [city, setCity] = useState('');
+  const [formValid, setFormValid] = useState(false);
 
-<Button component={CustomLink} to="/CustomerRegister" variant="contained" size="small" sx={{backgroundColor: 'rgb(13, 184, 13)', margin:'10px'}}>Tillbaka</Button>
-<Button component={CustomLink} to="/Payment"variant="contained" size="small" sx={{backgroundColor: 'rgb(13, 184, 13)', margin: '10px'}}>Nästa</Button>
-</Box>
+  const handleInputChange = () => {
+    if (address.trim() !== '' && postalCode.trim() !== '' && city.trim() !== '') {
+      setFormValid(true);
+    } else {
+      setFormValid(false);
+    }
+  };
+
+  return (
+    <div id="Orderingpage">
+      <div className="Register">
+        <h1>Vänligen ange dina uppgifter</h1>
+        <Box
+          display="flex"
+          flexDirection="column"
+          width="400px"
+          justifyContent="space-evenly"
+          padding="5px"
+        >
+          <TextField
+            required
+            label="Adress"
+            sx={{ bgcolor: 'white', borderRadius: '5px', marginTop: '35px' }}
+            value={address}
+            onChange={(e) => {
+              setAddress(e.target.value);
+              handleInputChange();
+            }}
+          ></TextField>
+          <TextField
+            type="number"
+            required
+            label="Postnummer"
+            sx={{ bgcolor: 'white', borderRadius: '5px', marginTop: '35px' }}
+            value={postalCode}
+            onChange={(e) => {
+              setPostalCode(e.target.value);
+              handleInputChange();
+            }}
+          ></TextField>
+          <TextField
+            required
+            label="Ort"
+            sx={{ bgcolor: 'white', borderRadius: '5px', marginTop: '35px', marginBottom: '127px' }}
+            value={city}
+            onChange={(e) => {
+              setCity(e.target.value);
+              handleInputChange();
+            }}
+          ></TextField>
+        </Box>
+        <Box display="flex" flexDirection="row" justifyContent="center">
+          <Button
+            component={Link} // Använd Link här istället för CustomLink
+            to="/CustomerRegister"
+            variant="contained"
+            size="small"
+            sx={{ backgroundColor: 'rgb(13, 184, 13)', margin: '10px' }}
+          >
+            Tillbaka
+          </Button>
+          <Button
+            component={Link} // Använd Link här istället för CustomLink
+            to="/Payment"
+            variant="contained"
+            size="small"
+            sx={{ backgroundColor: 'rgb(13, 184, 13)', margin: '10px' }}
+            disabled={!formValid}
+          >
+            Nästa
+          </Button>
+        </Box>
+      </div>
     </div>
-    </div> );
+  );
 }
 
 export default ShippingForm;
