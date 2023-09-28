95a8c3951a8dc92c7752cac45cc33703b9dcea49
Olof Hag <olof.hag123@gmail.com>
2023-09-28 13:56:12 +0200

    Added functionality for orderdata

--- src/admin/Order.tsx
+++ src/admin/Order.tsx
@@ -1,52 +1,92 @@
 import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
 
-interface Orders{
-    _id: number,
-    customerId: string,
-    products: string[],
-    totalCost: string,
-    payMethod: string,
-    payed: boolean,
-    isDone: boolean;
+interface Product {
+  name: string;
 }
+type PayMethod = kort | swish;
+interface Orders {
+  phone: number;
+  postCode: number;
+  city: string;
+  address: string;
+  _id: number;
+  customerId: string;
+  firstName: string;
+  lastName: string;
+  products: Product[]; 
+  totalCost: string;
+  payMethod: PayMethod;
+  payed: boolean;
+  isDone: boolean;
+}
+
 function AdminOrders() {
-    const [orders, setOrders] = useState<Orders[]>([]);
+  const [orders, setOrders] = useState<Orders[]>([]);
 
-    useEffect(() => {
-        fetch('http://localhost:3000/orderdata')
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            
-            if (Array.isArray(data)) {
-              setOrders(data);
-            } else {
-              console.error('Received data is not an array');
-              setOrders([]);
-            }
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-          });
-      }, []);
+  useEffect(() => {
+    fetch("http://localhost:3000/orderdata")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (Array.isArray(data)) {
+          console.log(data);
+          setOrders(data);
+        } else {
+          console.error("Received data is not an array");
+          setOrders([]);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-    return ( <>
-    <h1>admin orders</h1>
-    <Box>
+  return (
+    <>
+      <h1>admin orders</h1>
+      <Box>
         {orders.map((order) => (
-          <Box sx={{ borderBottom: 1 }}key={order._id}>
-            <h5>Kund id:{order.customerId}</h5>
-            <h5>Produkter:{order.products}</h5>
+          <Box sx={{ borderBottom: 1 }} key={order._id}>
+            <h2>Beställningsstatus</h2>
+            {order.payed ? (
+              <p>Beställningen är betald.</p>
+            ) : (
+              <p>Beställningen är inte betald.</p>
+            )}
+
+            {order.isDone ? <p>Ordern är klar</p> : <p>Ordern är inte klar</p>}
+            <h5>Kund id:{order._id}</h5>
+            <h5>Kundens Förnamn: {order.firstName}</h5>
+            <h5>Kundens Efternamn: {order.lastName}</h5>
+            <h5>Adress: {order.address}</h5>
+            <h5>Stad: {order.city}</h5>
+            <h5>Postnummer: {order.postCode}</h5>
+            <h5>Telefonnummer: {order.phone}</h5>
+
+           
+         {/*    <h5>
+  Produkter:
+  {order.products.map((product, index) => (
+    <div key={index}>
+      <h6>{product.name}</h6>
+      <p>Beskrivning: {product.description}</p>
+      <p>Pris: {product.price} kr</p>
+    </div>
+  ))}
+</h5> */}
+
             <h5>Kostnad:{order.totalCost}KR</h5>
-            <h5>Betalning metod:{order.payMethod}</h5>
-            <h5>Betalad:{order.payed}</h5>
-            <h5>Är ordern klar: {order.isDone}</h5>
+            <h5>
+  Betalnings metod:
+  {order.payMethod.swish ? "swish" : "kort"}
+</h5>
           </Box>
         ))}
       </Box>
-    </> );
+    </>
+  );
 }
 
 export default AdminOrders;
